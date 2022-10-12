a33db974d7cfe485c046b7e6b0f02840c1ea69f0
jorgeantonio21 <jtiago1993@gmail.com>
2022-10-12 12:20:12 +0100

    add data structures

--- contracts/src/lib.rs
+++ contracts/src/lib.rs
@@ -1,16 +1,27 @@
-use near_sdk::{near_bindgen};
+use near_sdk::{near_bindgen, AccountId,};
+use near_sdk::collections::{LookupMap, Vector};
+
+pub type CreatorAccountId = AccountId;
+pub type UserAccountId = AccountId;
 
 #[near_bindgen]
 pub struct Contract {
-    pub epoch: u16;
+    pub epoch: u16,
+    pub user_votes_mapping: LookupMap<UserAccountId, CreatorAccountId>,
+    pub creator_votes_mapping: LookupMap<CreatorAccountId, Vector<UserAccountId>>,
 }
 
 #[near_bindgen]
 impl Contract {
     #[init]
     pub fn new() -> Self {
+        let user_votes_mapping = LookupMap::<UserAccountId, CreatorAccountId>::new(b"a");
+        let creator_votes_mapping = LookupMap::<CreatorAccountId, Vector<UserAccountId>>::new(b"b");
+        
         Self {
             epoch: 0u16,
+            user_votes_mapping,
+            creator_votes_mapping,
         }
     }
 }
