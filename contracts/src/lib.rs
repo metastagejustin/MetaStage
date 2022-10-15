78ede0a61ef2a609d209141906b07b2f4952b4f5
jorgeantonio21 <jtiago1993@gmail.com>
2022-10-15 13:58:05 +0100

    add further methods

--- contracts/src/lib.rs
+++ contracts/src/lib.rs
@@ -1,8 +1,12 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::LookupMap;
 use near_sdk::serde::{Deserialize, Serialize};
-use near_sdk::{near_bindgen, AccountId, PanicOnDefault};
+use near_sdk::{env, near_bindgen, AccountId, PanicOnDefault};
 use std::collections::HashMap;
+
+use crate::error::MetaDaoError;
+
+mod error;
 
 pub type CreatorAccountId = AccountId;
 pub type UserAccountId = AccountId;
@@ -11,6 +15,16 @@ pub type FTAccountId = AccountId;
 #[derive(BorshDeserialize, BorshSerialize, Deserialize, Serialize)]
 #[serde(crate = "near_sdk::serde")]
 pub struct Epoch(u16);
+
+impl Epoch {
+    fn count(&self) -> u16 {
+        self.0
+    }
+
+    fn next_epoch(&mut self) {
+        self.0 += 1
+    }
+}
 
 #[derive(BorshDeserialize, BorshSerialize, Deserialize, Serialize, Clone, Debug)]
 #[serde(crate = "near_sdk::serde")]
@@ -22,16 +36,19 @@ pub struct TokenAmount {
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct MetaDaoContract {
+    pub admin: AccountId, // TODO: can we do it without an admin ?
     pub epoch: Epoch,
     pub user_votes_mapping: LookupMap<Epoch, HashMap<UserAccountId, CreatorAccountId>>,
     pub creator_votes_mapping: LookupMap<Epoch, HashMap<CreatorAccountId, Vec<UserAccountId>>>,
     pub creator_funds: LookupMap<Epoch, HashMap<CreatorAccountId, Vec<TokenAmount>>>,
+    pub user_funding: LookupMap<Epoch, HashMap<UserAccountId, TokenAmount>>,
+    pub is_epoch_on: bool,
 }
 
 #[near_bindgen]
 impl MetaDaoContract {
     #[init]
-    pub fn new() -> Self {
+    pub fn new(admin: AccountId) -> Self {
         let user_votes_mapping =
             LookupMap::<Epoch, HashMap<UserAccountId, CreatorAccountId>>::new(b"a".to_vec());
         let creator_votes_mapping =
@@ -40,11 +57,48 @@ impl MetaDaoContract {
         let creator_funds =
             LookupMap::<Epoch, HashMap<CreatorAccountId, Vec<TokenAmount>>>::new(b"c".to_vec());
 
+        let user_funding =
+            LookupMap::<Epoch, HashMap<UserAccountId, TokenAmount>>::new(b"d".to_vec());
+
         Self {
+            admin,
             epoch: Epoch(0u16),
             user_votes_mapping,
             creator_votes_mapping,
             creator_funds,
+            user_funding,
+            is_epoch_on: false,
         }
+    }
+
+    #[handle_result]
+    fn create_new_epoch(&mut self) -> Result<(), MetaDaoError> {
+        if env::predecessor_account_id() != self.admin {
+            return Err(MetaDaoError::InvalidAdminCall);
+        }
+
+        if self.is_epoch_on {
+            return Err(MetaDaoError::UnableToCreatNewEpoch);
+        }
+
+        // update epoch
+        self.epoch.next_epoch();
+
+        // create new entries for other contract fields, for new epoch
+        self.user_votes_mapping
+            .insert(&self.epoch, &HashMap::<UserAccountId, CreatorAccountId>::new());
+        self.creator_votes_mapping.insert(
+            &self.epoch,
+            &HashMap::<CreatorAccountId, Vec<UserAccountId>>::new(),
+        );
+        self.creator_funds.insert(
+            &self.epoch,
+            &HashMap::<CreatorAccountId, Vec<TokenAmount>>::new(),
+        );
+        self.user_funding
+            .insert(&self.epoch, &HashMap::<UserAccountId, TokenAmount>::new());
+        self.is_epoch_on = true;
+
+        Ok(())
     }
 }
