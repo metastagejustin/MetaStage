813216f93a1f5af6f6f213118010843fd20c782b
jorgeantonio21 <matroid@outlook.com>
2022-10-22 19:28:33 +0100

    add register user logic

--- contracts/src/error.rs
+++ contracts/src/error.rs
@@ -8,9 +8,34 @@ pub enum MetaDaoError {
     #[error("Invalid Admin call")]
     InvalidAdminCall,
     #[error("Unable to create a new epoch, while previous epoch is still ongoing")]
-    UnableToCreatNewEpoch,
+    UnableToCreateNewEpoch,
     #[error("Creator is not registered for current epoch")]
     CreatorIsNotRegistered,
     #[error("Invalid current epoch")]
     InvalidCurrentEpoch,
+    #[error("Currently, epoch is off")]
+    EpochIsOff,
+    #[error("Not in funding period")]
+    NotInFundingPeriod,
+    #[error("User did not attach enough funds to contract call")]
+    UserDidNotAttachEnoughFunds,
+    #[error("User already registered funds to creator")]
+    UserAlreadyRegisteredFundsToCreator,
+}
+
+impl AsRef<str> for NearConLiqError {
+    fn as_ref(&self) -> &str {
+        match self {
+            Self::InvalidAdminCall => "Invalid Admin call",
+            Self::UnableToCreateNewEpoch => {
+                "Unable to create a new epoch, while previous epoch is still ongoing"
+            }
+            Self::CreatorIsNotRegistered => "Creator is not registered for current epoch",
+            Self::InvalidCurrentEpoch => "Invalid current epoch",
+            Self::EpochIsOff => "Currently, epoch is off",
+            Self::NotInFundingPeriod => "Not in funding period",
+            Self::UserDidNotAttachEnoughFunds => "User did not attach enough funds to contract call",
+            Self::UserAlreadyRegisteredfundsToCreator => "User already registered funds to creator",
+        }
+    }
 }
